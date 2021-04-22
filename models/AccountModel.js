0531bc36c1d1fe120f4333819e51e2837edd4110
Vicente Silva <vicente.pspenha@gmail.com>
2021-04-21 22:05:20 -0300

    Scheme created for account.

---
+++ models/AccountModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema({
+  agencia: { 
+    type: String,
+    require: true
+  },
+  conta: { 
+    type: Number,
+    require: true
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  balance: {
+     type: Number,
+     min: 0,
+     require: true
+  }
+});
+
+const accountModel = mongoose.model('account', accountSchema, 'account');
+
+export {accountModel};
