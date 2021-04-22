e005b963917bff1257f5b7d7880ac4217829f10a
Vicente Silva <vicente.pspenha@gmail.com>
2021-04-22 01:36:09 -0300

    MongoDB connection information removed, moved to configuration file.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import * as fs from'fs';
 
 import {accountRouter} from './routes/AccountRouter.js'
 
@@ -9,17 +10,26 @@ const app = express();
 app.use(express.json());
 app.use('/accounts', accountRouter);
 
+let urlMongoDB = '';
+const fileUrl = async () => {
+    await fs.readFile('config.json', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        const fileConfig = JSON.parse(data);
+        urlMongoDB = fileConfig.urlMongoDB;
+        connectMongoDB();
+    });
+}
+fileUrl();
 
-const URL =
- 'mongodb+srv://vicente:2021@cluster0.8nhao.mongodb.net/desafio01?retryWrites=true&w=majority';
-const connect = async () => {
-    await mongoose.connect(URL, {
+const connectMongoDB = async () => {
+    await mongoose.connect(urlMongoDB, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
     console.log('Conectado ao MongDB')
 }
-connect();
 
 
 app.listen(3000, () => {
