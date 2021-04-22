7f9e83c2bce91065facba9ddfff03f15c7379daf
Vicente Silva <vicente.pspenha@gmail.com>
2021-04-22 01:51:03 -0300

    Routes created to insert and remove registration.

--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -4,6 +4,18 @@ import {accountModel} from '../models/AccountModel.js'
 
 const app = express();
 
+
+app.post('', async (req, res) => {
+    try {
+        const account = new accountModel(req.body);
+        await account.save();
+        res.status(200).send(account);
+    } catch  (error) {
+        console.log('error => ', error)
+        res.status(500).send();
+    }
+})
+
 // Task item four.
 app.patch('/deposit/:agencia/:conta/:valor', async (req, res) => {
     try {
@@ -60,5 +72,24 @@ app.get('/balance/:agencia/:conta', async (req, res) => {
         res.status(500).send();
     }
 })
+
+// Task item seven.
+app.delete('/:agencia/:conta', async (req, res) => {
+    try {
+        const {agencia, conta} = req.params;
+        const account = await accountModel.findOneAndDelete({$and: [{agencia}, {conta}]});
+        console.log(account);
+        if (!account) {
+            res.status(404).send('Account not found');
+        } else {
+            const agencias = await accountModel.find({agencia}, {_id: 1});
+            res.status(200).send(agencias.length.toString());
+        }
+    } catch  (error) {
+        console.log('error => ', error)
+        res.status(500).send();
+    }
+})
+
 
 export {app as accountRouter};
