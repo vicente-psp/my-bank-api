63aedfab0dd786c2d10c4f4a53f73493f55106b8
Vicente Silva <vicente.pspenha@gmail.com>
2021-04-22 00:21:47 -0300

    Created route for withdrawals.

--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -4,6 +4,7 @@ import {accountModel} from '../models/AccountModel.js'
 
 const app = express();
 
+// Task item four.
 app.patch('/deposit/:agencia/:conta/:valor', async (req, res) => {
     try {
         const {agencia, conta, valor} = req.params;
@@ -14,6 +15,29 @@ app.patch('/deposit/:agencia/:conta/:valor', async (req, res) => {
             account.balance += (valor * 1);
             await account.save();
             res.status(200).send(`Current balance: ${account.balance}`);
+        }
+    } catch  (error) {
+        console.log('error => ', error)
+        res.status(500).send();
+    }
+})
+
+// Task item five.
+app.patch('/withdraw/:agencia/:conta/:valor', async (req, res) => {
+    try {
+        const {agencia, conta, valor} = req.params;
+        const account = await accountModel.findOne({$and: [{agencia}, {conta}]});
+        if (!account) {
+            res.status(404).send('Account not found');
+        } else {
+            const TARIFA = 1;
+            account.balance -= ((valor * 1) + TARIFA);
+            if (account.balance >= 0) {
+                await account.save();
+                res.status(200).send(`Current balance: ${account.balance}`);
+            } else {
+                res.status(401).send(`Insufficient balance`);
+            }
         }
     } catch  (error) {
         console.log('error => ', error)
