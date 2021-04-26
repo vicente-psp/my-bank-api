63d1365f139a3bfc637ae0286dcff828da269c0c
Vicente Silva <vicente.pspenha@gmail.com>
2021-04-25 23:44:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,22 +11,8 @@ dotenv.config();
 app.use(express.json());
 app.use('/accounts', accountRouter);
 
-/*
-let urlMongoDB = '';
-const fileUrl = async () => {
-    await fs.readFile('config.json', (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        const fileConfig = JSON.parse(data);
-        urlMongoDB = fileConfig.urlMongoDB;
-        connectMongoDB();
-    });
-}
-fileUrl();
-*/
-
-let urlMongoDB = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.8nhao.mongodb.net/desafio01?retryWrites=true&w=majority`;
+let urlMongoDB = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}`;
+urlMongoDB += `@cluster0.8nhao.mongodb.net/desafio01?retryWrites=true&w=majority`;
 const connectMongoDB = async () => {
     await mongoose.connect(urlMongoDB, {
         useNewUrlParser: true,
