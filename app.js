311c65793c1423c5b2c2f430327418476a7ca11a
Vicente Silva <vicente.pspenha@gmail.com>
2021-04-25 23:41:07 -0300

    Encapsulate environment variables with 'dotenv'.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import * as fs from'fs';
+import dotenv from 'dotenv';
 
 import {accountRouter} from './routes/AccountRouter.js'
 
 const app = express();
-
+dotenv.config();
 
 app.use(express.json());
 app.use('/accounts', accountRouter);
 
+/*
 let urlMongoDB = '';
 const fileUrl = async () => {
     await fs.readFile('config.json', (err, data) => {
@@ -22,7 +24,9 @@ const fileUrl = async () => {
     });
 }
 fileUrl();
+*/
 
+let urlMongoDB = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.8nhao.mongodb.net/desafio01?retryWrites=true&w=majority`;
 const connectMongoDB = async () => {
     await mongoose.connect(urlMongoDB, {
         useNewUrlParser: true,
@@ -30,8 +34,9 @@ const connectMongoDB = async () => {
     });
     console.log('Conectado ao MongDB')
 }
+connectMongoDB();
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('API iniciada');
 })
